c8ad89864f88ffef7115e5fe98965a8f85144df7
lalon <lalon@WorldAlonso>
2026-05-01 16:37:05 -0600

    clase conexion finalizada

--- ProyectoFinalPoo/src/main/java/configConexion/Conexion.java
+++ ProyectoFinalPoo/src/main/java/configConexion/Conexion.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package configConexion;
-
-/**
- *
- * @author lalon
- */
-public class Conexion {
-    int clase;
-
-    public Conexion(int clase) {
-        this.clase = clase;
+package ConfigConexion;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+public class Conexion { 
+    private Connection con;
+    private final String url = "jdbc:sqlserver://localhost\\Ferreteria:1433;databaseName=FerreteriaNueva;encrypt=true;trustServerCertificate=true";
+    private final String user = "LogProyectoPOO";
+    private final String pass = "12345";
+ 
+    public Connection conectar() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("✅ Conexion establecida con exito.");
+        } catch (ClassNotFoundException e) {
+            System.out.println(" Error: Driver JDBC no encontrado.");
+        } catch (SQLException e) {
+            System.out.println(" Error de SQL: " + e.getMessage());
+        }
+        return con;
     }
-    
-    
-     
+ 
+    public static void main(String[] args) {
+        Conexion c = new Conexion();
+        c.conectar();
+    }
 }
